a989d0a0ead5ae3032cf14c6641e00070a4c52a1
Zintom <tzsinkoapps@gmail.com>
2023-04-30 00:35:34 +0100

    Create IHttpClient and default wrapper.
    This is so that we can test functionality without actually calling the real API.

---
+++ OpenAIWrapper/OpenAIWrapper/HttpClientWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Net.Http;
+
+namespace Zintom.OpenAIWrapper;
+public class HttpClientWrapper : HttpClient, IHttpClient
+{
+    public void GetStreamingResponse(HttpRequestMessage request, out HttpResponseMessage response, out Stream responseStream)
+    {
+        response = Send(request, HttpCompletionOption.ResponseHeadersRead);
+        responseStream = response.Content.ReadAsStream();
+    }
+}
---
+++ OpenAIWrapper/OpenAIWrapper/IHttpClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Zintom.OpenAIWrapper;
+
+public interface IHttpClient
+{
+
+    public HttpRequestHeaders DefaultRequestHeaders { get; }
+
+    public Task<HttpResponseMessage> PostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? requestUri, HttpContent? content);
+
+    // Intended to wrap the 'HttpClient.Send' function, allowing mocking.
+    public void GetStreamingResponse(HttpRequestMessage request, out HttpResponseMessage response, out Stream responseStream);
+
+}
