0501c8335fbf73a38d5dfb8e5aa5ded270df88ad
Zintom <tzsinkoapps@gmail.com>
2023-04-30 00:34:59 +0100

    Add Delta field

--- OpenAIWrapper/OpenAIWrapper/Models/ChatCompletion.cs
+++ OpenAIWrapper/OpenAIWrapper/Models/ChatCompletion.cs
@@ -41,9 +41,30 @@ public class Choice
     [JsonPropertyName("message")]
     public Message? Message { get; set; }
 
+    [JsonPropertyName("delta")]
+    public Message? Delta { get; set; }
+
     [JsonPropertyName("finish_reason")]
     public string? FinishReason { get; set; }
 
     [JsonPropertyName("index")]
     public int Index { get; set; }
+}
+
+/// <summary>
+/// A message as a part of a conversation.
+/// </summary>
+public class Message
+{
+    /// <summary>
+    /// The role of the author of this message. One of '<i>system</i>', '<i>user</i>', or '<i>assistant</i>'.
+    /// </summary>
+    [JsonPropertyName("role")]
+    public string? Role { get; set; }
+
+    /// <summary>
+    /// The contents of the message.
+    /// </summary>
+    [JsonPropertyName("content")]
+    public string? Content { get; set; }
 }
