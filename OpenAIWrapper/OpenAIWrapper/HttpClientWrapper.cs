c17f02de8bf65134b4d5cca67234c0f9a310c24e
Zintom <tzsinkoapps@gmail.com>
2023-04-30 13:48:44 +0100

    Add code docs.

--- OpenAIWrapper/OpenAIWrapper/HttpClientWrapper.cs
+++ OpenAIWrapper/OpenAIWrapper/HttpClientWrapper.cs
@@ -2,8 +2,13 @@
 using System.Net.Http;
 
 namespace Zintom.OpenAIWrapper;
+
+/// <summary>
+/// A wrapper around the <see cref="HttpClient"/>, implements <see cref="IHttpClient"/>.
+/// </summary>
 public sealed class HttpClientWrapper : HttpClient, IHttpClient
 {
+    /// <inheritdoc cref="IHttpClient.GetStreamingResponse(HttpRequestMessage, out HttpResponseMessage, out Stream)"/>
     public void GetStreamingResponse(HttpRequestMessage request, out HttpResponseMessage response, out Stream responseStream)
     {
         response = Send(request, HttpCompletionOption.ResponseHeadersRead);
--- OpenAIWrapper/OpenAIWrapper/IHttpClient.cs
+++ OpenAIWrapper/OpenAIWrapper/IHttpClient.cs
@@ -6,14 +6,24 @@ using System.Threading.Tasks;
 
 namespace Zintom.OpenAIWrapper;
 
+/// <summary>
+/// Exposes <see cref="HttpClient"/> as an interface.
+/// </summary>
 public interface IHttpClient
 {
 
+    /// <inheritdoc cref="HttpClient.DefaultRequestHeaders"/>
     public HttpRequestHeaders DefaultRequestHeaders { get; }
 
+    /// <inheritdoc cref="HttpClient.PostAsync(string?, HttpContent?)"/>
     public Task<HttpResponseMessage> PostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? requestUri, HttpContent? content);
 
-    // Intended to wrap the 'HttpClient.Send' function, allowing mocking.
+    /// <summary>
+    /// Sends the given <paramref name="request"/> and gets the response data as a stream.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="response"></param>
+    /// <param name="responseStream"></param>
     public void GetStreamingResponse(HttpRequestMessage request, out HttpResponseMessage response, out Stream responseStream);
 
 }
