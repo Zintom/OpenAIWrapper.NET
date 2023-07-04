1b76d35502585da280c9ad3805eec819ee0cfbd4
Zintom <tzsinkoapps@gmail.com>
2023-07-04 12:06:41 +0100

    Add "Name" property to model.

--- OpenAIWrapper/OpenAIWrapper/Models/ChatCompletion.cs
+++ OpenAIWrapper/OpenAIWrapper/Models/ChatCompletion.cs
@@ -101,6 +101,12 @@ public sealed class Message
     /// </summary>
     [JsonPropertyName("content")]
     public string? Content { get; set; }
+
+    /// <summary>
+    /// The name of the author of this message. May contain a-z, A-Z, 0-9, and underscores, with a maximum length of 64 characters.
+    /// </summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
 }
 
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
