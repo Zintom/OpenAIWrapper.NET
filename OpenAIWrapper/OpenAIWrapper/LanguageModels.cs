4ae513aa0035602af90bdaa44ea415899e791533
Zintom <tzsinkoapps@gmail.com>
2023-07-04 14:15:29 +0100

    Create LanguageModels.cs

---
+++ OpenAIWrapper/OpenAIWrapper/LanguageModels.cs
@@ -0,0 +1,13 @@
+﻿namespace Zintom.OpenAIWrapper;
+
+public static class LanguageModels
+{
+
+    public static string gpt_3_5_turbo = "gpt-3.5-turbo";
+
+    /// <summary>
+    /// Function calling capability.
+    /// </summary>
+    public static string gpt_3_5_turbo_0613 = "gpt-3.5-turbo-0613";
+
+}
