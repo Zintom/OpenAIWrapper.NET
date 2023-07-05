6ddac79afbee1fc67cdc4fe9ac26bd2c2d4827ac
Zintom <tzsinkoapps@gmail.com>
2023-07-05 13:12:20 +0100

    Add GPT-4

--- OpenAIWrapper/OpenAIWrapper/LanguageModels.cs
+++ OpenAIWrapper/OpenAIWrapper/LanguageModels.cs
@@ -1,13 +1,29 @@
 ﻿namespace Zintom.OpenAIWrapper;
 
+/// <summary>
+/// List of available language models.
+/// </summary>
 public static class LanguageModels
 {
 
-    public static string gpt_3_5_turbo = "gpt-3.5-turbo";
+    /// <summary>
+    /// GPT-3.5
+    /// </summary>
+    public static string GPT_3_5_Turbo { get; } = "gpt-3.5-turbo";
 
     /// <summary>
-    /// Function calling capability.
+    /// GPT-3.5 with function calling capability.
     /// </summary>
-    public static string gpt_3_5_turbo_0613 = "gpt-3.5-turbo-0613";
+    public static string GPT_3_5_Turbo_0613 { get; } = "gpt-3.5-turbo-0613";
+
+    /// <summary>
+    /// GPT-4
+    /// </summary>
+    public static string GPT_4 { get; } = "gpt-4";
+
+    /// <summary>
+    /// GPT-4 with function calling capability.
+    /// </summary>
+    public static string GPT_4_0613 { get; } = "gpt-4-0613";
 
 }
